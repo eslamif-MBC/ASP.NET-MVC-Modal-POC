8bda87bbd081893465d6dd2228d933a170e09175
Frank Eslami <frank.eslami@mbc.ca.gov>
2023-02-28 08:27:41 -0800

    Upgraded to latest Bootstrap 5.2.

--- MvcModalPOC/App_Start/BundleConfig.cs
+++ MvcModalPOC/App_Start/BundleConfig.cs
@@ -19,8 +19,9 @@ namespace MvcModalPOC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+                "~/Scripts/umd/popper.min.js",
+                "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
